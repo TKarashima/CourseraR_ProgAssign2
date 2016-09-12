20c574d6a811e6063545284c5d28c7cf45782306
TKarashima <thiagomk@gmail.com>
2016-09-12 12:15:55 -0300

    Testing GitHub.1
    This is a test of my Github

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,8 @@
 # se made by cacheSolve(), and for returning a saved inverse after queried by
 # cacheSolve().
 
+# THIS IS A GITHUB TEST
+
 makeCacheMatrix <- function(x = matrix()) {
         ii <- NULL
         set <- function(y) {
